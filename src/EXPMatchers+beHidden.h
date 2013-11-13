4205a9db3eff40b94f2fea7c1d706b71fe05b51b
Klaas Pieter Annema <klaaspieter@annema.me>
2013-11-13 14:31:54 -0500

    Add isHidden matcher

---
+++ src/EXPMatchers+beHidden.h
@@ -0,0 +1,14 @@
+//
+//  EXPMatchers+beHidden.h
+//  UISpecta
+//
+//  Created by Klaas Pieter Annema on 07-11-13.
+//  Copyright (c) 2013 Annema. All rights reserved.
+//
+
+
+#import <UIKit/UIKit.h>
+
+#import "Expecta.h"
+
+EXPMatcherInterface(beHidden, (void));
---
+++ src/EXPMatchers+beHidden.m
@@ -0,0 +1,44 @@
+//
+//  EXPMatchers+beHidden.m
+//  UISpecta
+//
+//  Created by Klaas Pieter Annema on 13-11-13.
+//  Copyright (c) 2013 Annema. All rights reserved.
+//
+
+#import "EXPMatchers+beHidden.h"
+
+#import "UISpectaSupport.h"
+
+EXPMatcherImplementationBegin(beHidden, (void)) {
+  BOOL actualIsCompatible = [actual respondsToSelector:@selector(isHidden)] && [actual respondsToSelector:@selector(alpha)];
+
+  prerequisite(^BOOL {
+    return actualIsCompatible;
+  });
+
+  match(^BOOL {
+    if (!actualIsCompatible) {
+      return NO;
+    } else {
+      return [actual isHidden] || [actual alpha] <= 0.0;
+    }
+  });
+
+  failureMessageForTo(^NSString * {
+    if (!actualIsCompatible) {
+      return [NSString stringWithFormat:@"expected: %@ to respond to isHidden and alpha, but it doesn't", UISDescribeObject(actual)];
+    } else {
+      return [NSString stringWithFormat:@"expected: %@ to be hidden, but it's visible", UISDescribeObject(actual)];
+    }
+  });
+
+  failureMessageForNotTo(^NSString * {
+    if (!actualIsCompatible) {
+      return [NSString stringWithFormat:@"expected: %@ to respond to isHidden and alpha, but it doesn't", UISDescribeObject(actual)];
+    } else {
+      return [NSString stringWithFormat:@"expected: %@ to be visible, but it's hidden", UISDescribeObject(actual)];
+    }
+  });
+}
+EXPMatcherImplementationEnd
